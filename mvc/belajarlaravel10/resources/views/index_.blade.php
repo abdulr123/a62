edd07bae8b2cf3a90002ba91f87b02df4065c62f
Abdul Rahman <tugas.abdulrahman@gmail.com>
2024-03-21 07:16:08 +0700

    add laravel breeze

---
+++ mvc/belajarlaravel10/resources/views/index_.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Ini file INDEX</title>
+</head>
+<body>
+    <h1>Halaman Pertama Belajar Laravel</h1>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Perspiciatis minima amet, asperiores adipisci tenetur voluptatum commodi rerum illum consequatur reiciendis sunt. Error at explicabo placeat, sapiente amet inventore. Tempore, nulla!</p>
+</body>
+</html>
